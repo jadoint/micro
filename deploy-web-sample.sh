0dd496da87590eafb92a1322a4c090f6f146afac
jadointeractive <david@jadointeractive.com>
2019-09-12 01:18:26 -0700

    Updated deploy-web script to minimize downtime.

--- deploy-web-sample.sh
+++ deploy-web-sample.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # Upload to server
-ssh host 'rm -rf web;mkdir -p web'
+ssh host 'rm -rf web/public;rm -rf web/src;rm web/package.json'
 scp -r web/public user@host:~/web/public &
 scp -r web/src user@host:~/web/src &
 scp -r web/package.json user@host:~/web/package.json &
 wait
 
-ssh host 'cd web;yarn install;yarn build'
+# Install node modules
+ssh host 'cd web;yarn install'
+
+# Leave build directory last to minimize site downtime
+ssh host 'rm -rf web/build'
+ssh host 'cd web;yarn build'
 
 echo "All done"
