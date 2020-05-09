1bd3b5d372813f521f3dae245c92fb066a32811c
jadointeractive <david@jadointeractive.com>
2020-05-08 23:53:40 -0700

    Added msqqueue package as a Kafka helper.

--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/go-chi/cors v1.0.0
 	github.com/go-playground/locales v0.13.0
 	github.com/go-playground/universal-translator v0.17.0
-	github.com/go-playground/validator v9.30.0+incompatible
+	github.com/go-playground/validator v9.30.0+incompatible // indirect
 	github.com/go-playground/validator/v10 v10.0.1
 	github.com/go-redis/redis v6.15.6+incompatible
 	github.com/go-redis/redis/v7 v7.0.0-beta.4
@@ -24,5 +24,5 @@ require (
 	golang.org/x/sys v0.0.0-20191023151326-f89234f9a2c2 // indirect
 	golang.org/x/time v0.0.0-20191023065245-6d3f0bb11be5 // indirect
 	google.golang.org/appengine v1.6.5 // indirect
-	gopkg.in/go-playground/validator.v9 v9.30.0
+	gopkg.in/go-playground/validator.v9 v9.30.0 // indirect
 )
---
+++ pkg/msgqueue/msgqueue.go
@@ -0,0 +1,48 @@
+package msgqueue
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/jadoint/micro/pkg/logger"
+)
+
+// SendMsg send message to Kafka
+func SendMsg(topic string, msg []byte) {
+	kafkaServer := os.Getenv("KAFKA_SERVER")
+	if kafkaServer == "" {
+		logger.LogError(errors.New("KAFKA_SERVER is not set"))
+		return
+	}
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": kafkaServer})
+	if err != nil {
+		panic(err)
+	}
+
+	defer p.Close()
+
+	// Delivery report handler for produced messages
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					logger.LogError(fmt.Errorf("Delivery failed: %v", ev.TopicPartition))
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
+	// Produce messages to topic (asynchronously)
+	p.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          msg,
+	}, nil)
+
+	// Wait for message deliveries before shutting down
+	p.Flush(15 * 1000)
+}
