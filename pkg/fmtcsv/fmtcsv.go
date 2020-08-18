9820d344bd152cecfce4628b19d0d8ad7f209b8c
jadointeractive <david@jadointeractive.com>
2020-08-18 00:39:27 -0700

    Now returning an error for MakeCSVFromList().

--- pkg/fmtcsv/fmtcsv.go
+++ pkg/fmtcsv/fmtcsv.go
@@ -1,14 +1,15 @@
 package fmtcsv
 
 import (
+	"errors"
 	"strconv"
 	"strings"
 )
 
 // MakeCSVFromList turns an int64 list into a csv string
-func MakeCSVFromList(nums []int64) string {
+func MakeCSVFromList(nums []int64) (string, error) {
 	if len(nums) == 0 {
-		return ""
+		return "", errors.New("Empty CSV")
 	}
 	var b strings.Builder
 	for _, v := range nums {
@@ -16,5 +17,5 @@ func MakeCSVFromList(nums []int64) string {
 	}
 	csv := b.String()
 	csv = csv[0 : len(csv)-1]
-	return csv
+	return csv, nil
 }
