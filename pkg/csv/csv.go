5e68906f7a2f27d84467703d9d34c6b01761800b
jadointeractive <david@jadointeractive.com>
2020-08-18 00:30:20 -0700

    Added utility function to convert an int64 list into a CSV string using strings.Builder.

---
+++ pkg/csv/csv.go
@@ -0,0 +1,20 @@
+package csv
+
+import (
+	"strconv"
+	"strings"
+)
+
+// MakeCSVFromList turns an int64 list into a csv string
+func MakeCSVFromList(nums []int64) string {
+	if len(nums) == 0 {
+		return ""
+	}
+	var b strings.Builder
+	for _, v := range nums {
+		b.WriteString(strconv.FormatInt(v, 10) + ",")
+	}
+	csv := b.String()
+	csv = csv[0 : len(csv)-1]
+	return csv
+}
